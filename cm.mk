21dc691181dc7cc015a7e38cafeab2b735c94d53
Lorenzo <fonz993@hotmail.it>
2015-02-08 11:23:52 +0100

    remove deprecated gsm.mk

--- cm.mk
+++ cm.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Inherit some common CM stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
